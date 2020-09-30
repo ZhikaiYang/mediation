11dec9bf465bbdb10ee4a979638508dcd589a0c9
zhikaiyang <zhikaiyang911@gmail.com>
2020-09-30 14:13:21 -0500

    trial of small data set

--- profiling/2_mediation/2.B.1_setup_med_job.R
+++ profiling/2_mediation/2.B.1_setup_med_job.R
@@ -29,7 +29,7 @@ df <- read.csv("largedata/df_job_control.csv")
 
 slurm_med_wrapper(ti=as.numeric(as.character(df$trait[JOBID])), cutoff_pm=0.05, ncores=8,
                   phenofile = "data/geno_trait.txt",
-                  genofile = "largedata/geno/allchr_bisnp_n282_snpid_maf01_geno2_pruned_NA_0_matrix.txt",
+                  genofile = "largedata/geno/gddtosilk_p1msnps_matrix.txt",
                   rnafile = as.character(df$rnafile[JOBID]),
                   pcfile="largedata/allchr_bisnp_n282_snpid_maf01_geno2_pruned.eigenvec",
                   outdir="largedata/med_output")
