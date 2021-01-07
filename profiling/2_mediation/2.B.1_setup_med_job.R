c1f616036b4ea8f745a74239bcd9a71c6a5e7ae5
zhikaiyang <zhikaiyang911@gmail.com>
2021-01-07 16:33:55 -0600

    trial of new geno data for agronomic traits

--- profiling/2_mediation/2.B.1_setup_med_job.R
+++ profiling/2_mediation/2.B.1_setup_med_job.R
@@ -25,13 +25,13 @@ source('lib/MedWrapper.R')
 source('lib/reporters.R')
 source('lib/slurm_wrapper.R')
 
-df <- read.csv("largedata/df_job_control_N_300T.csv")
+df <- read.csv("largedata/df_job_control_ag.csv")
 
 slurm_med_wrapper(ti=as.numeric(as.character(df$trait[JOBID])), cutoff_pm=0.05, ncores=16,
-                  phenofile = "data/blup_N_300_tax_groups.txt",
-                  genofile = "largedata/geno/gddtosilk_p7msnps_matrix.txt",
+                  phenofile = "data/geno_trait.txt",
+                  genofile = "largedata/geno/gddtosilk_p7msnps_matrix_new.txt",
                   rnafile = as.character(df$rnafile[JOBID]),
-                  pcfile="largedata/allchr_bisnp_n282_snpid_maf01_geno2_pruned.eigenvec",
+                  pcfile="largedata/geno/hmp321_282_agpv4_maf005_miss03_pruned.eigenvec",
                   outdir="largedata/med_output")
 
 #####
